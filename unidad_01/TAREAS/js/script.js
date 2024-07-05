9a9f2e8a1857d5251fc86d15c4d5cadcf87dc344
AnthonyAlava05 <anthony_alre@hotmail.com>
2024-07-05 00:45:37 -0500

    tarea terminada

---
+++ unidad_01/TAREAS/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById('cargar').addEventListener('click', function() {
+    const img = document.getElementById('image');
+    img.style.display = 'block';
+});
+
+document.getElementById('eliminar').addEventListener('click', function() {
+    const img = document.getElementById('image');
+    img.style.display = 'none';
+});
