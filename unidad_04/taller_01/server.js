ca372317d32161aca6c617630ec26610846f3d7d
AnthonyAlava05 <anthony_alre@hotmail.com>
2024-08-03 11:43:06 -0500

    ccc

--- unidad_04/taller_01/server.js
+++ unidad_04/taller_01/server.js
@@ -1,1 +1,13 @@
 const express = require('express')
+
+var app = express()
+
+app.use('/',function(req, res){
+    res.send('Hola mundo')
+})
+
+
+let puerto = 3000
+app.listen(3000)
+
+console.log(`La aplicación se encuentra funcionando en http://localhost:${puerto}/`)
